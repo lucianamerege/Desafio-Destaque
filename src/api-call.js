2ce64c6496ef54ee8f1d08c9a639c8b11fae6dcb
Luciana <52220617+lucianamerege@users.noreply.github.com>
2022-04-04 12:39:11 -0300

    feat(api) linguagens de cada repositorio

--- src/api-call.js
+++ src/api-call.js
@@ -1,28 +1,41 @@
-var form = document.getElementById("form")
-var search = document.getElementById("searchTerm")
-var cardHolder = document.getElementById("card-holder")
+var form = document.getElementById("form");
+var search = document.getElementById("searchTerm");
+var cardHolder = document.getElementById("card-holder");
+var pagination = document.getElementById("paginacao");
+var page = 1;
+var pageQuant;
+
+var api = "https://api.github.com/"
 
 form.addEventListener('submit', function(e){
 
   e.preventDefault()
-  var searchTerm = search.value
+  var searchTerm = search.value;
 
   //Limpar todos os cards da tela
   cleanCards();
 
+
+  //cleanPagination()
+
+
   //Removendo espaços em branco da string
-  searchTerm = searchTerm.split(' ').join('')
+  searchTerm = searchTerm.split(' ').join('');
 
   getRepositoryList(searchTerm);
 })
 
 //Função de chamada da API Search
 function getRepositoryList(searchTerm) {
-  fetch("https://api.github.com/search/repositories?q="+searchTerm+"&sort=stars&order=desc")
+  fetch(api + "search/repositories?q="+searchTerm+"&sort=stars&order=desc")
   .then((result) => result.json())
   .then((data) => {
-    createCards(data)
-    console.log(data)
+    createCards(data);
+
+    //Para mostrar quantos resultados teve a pesquisa e a paginação
+    //showPagination(data.total_count);
+
+    console.log(data);
   })
 
 }
@@ -35,16 +48,18 @@ function createCards(data){
   //Agora todos os resultados serão inclusos em um clone do template e adicionados ao "Card Holder"
   for(var i = 0; i<5; i++){
     //Deixando a data de atualizacao mais legível
-    var atualizacao = data.items[i].updated_at.split("T")
+    var atualizacao = data.items[i].updated_at.split("T");
 
     clone = template.content.cloneNode(true);
     clone.getElementById("name").textContent += data.items[i].name;
     clone.getElementById("description").textContent += data.items[i].description;
     clone.getElementById("author").textContent += data.items[i].owner.login;
-    clone.getElementById("language").textContent += data.items[i].languages_url;
     clone.getElementById("forks").textContent += data.items[i].forks_count;
     clone.getElementById("stars").textContent += data.items[i].stargazers_count;
     clone.getElementById("date").textContent += atualizacao[0];
+    //Alterando esse atributo customizado, vou poder acessa-lo mais tarde para descobrir qual card chamou a função que mostra as linguagens
+    clone.getElementById("show-languages").setAttribute('hidden-full-name', data.items[i].full_name);
+    clone.getElementById("show-languages").setAttribute('hidden-card-id', i);
 
     cardHolder.appendChild(clone);
   }
@@ -56,4 +71,59 @@ function cleanCards(){
   while (cardHolder.lastChild) {
     cardHolder.removeChild(cardHolder.lastChild);
   }
+}
+function cleanPagination(){
+  while (pagination.lastChild) {
+    pagination.removeChild(pagination.lastChild);
+  }
+}
+
+//Quantos resultados vieram da busca
+function showPagination(quantity){
+  var template, clone;
+
+  template = document.querySelector('#pagina-template');
+  clone = template.content.cloneNode(true);
+  clone.getElementById("quantidade").textContent += quantity;
+  
+  if(quantity%5 == 0){
+    pageQuant = quantity/5;
+  }else{
+    pageQuant = quantity+1;
+  }
+
+  pagination.appendChild(clone);
+}
+
+//Essa função faz uma segunda chamada da API, para pegar apenas as linguagens do repositorio selecionado.
+//Eu escolhi fazer isso separadamente para não abusar da API e aumentar as chances de me manter dentro do limite para chamadas sem autenticação,
+//já que uma não era necessária para implementar esse desafio.
+function showLanguages(e){
+  var fullName = e.target.getAttribute("hidden-full-name");
+  var cardId = e.target.getAttribute("hidden-card-id");
+  //mudando o botão para mostrar que foi clicado
+  e.target.textContent = "";
+  e.target.textContent = "▼";
+
+  fetch(api+"repos/"+fullName+"/languages")
+  .then((result) => result.json())
+  .then((data) => {
+    var languagesArray = Object.keys(data);
+    if(languagesArray.length == 0){
+      alertNoLanguages(cardId);
+    }else{
+      alertLanguages(languagesArray, cardId);
+    }
+  })
+}
+
+function alertLanguages(languagesArray, cardId){
+  var languageP = cardHolder.children[cardId].getElementsByClassName("linguagens-info")
+  languageP[0].textContent = languagesArray;
+  languageP[0].removeAttribute('hidden');
+}
+function alertNoLanguages(cardId){
+  var languageP = cardHolder.children[cardId].getElementsByClassName("linguagens-info")
+  languageP[0].textContent = "Linguages não especificadas";
+  languageP[0].removeAttribute('hidden');
 }
